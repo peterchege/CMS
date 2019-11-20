f7a603b715c07347670653fda4b8a0e59a4a4c3e
anthony-baru <anthonybaru@gmail.com>
2019-11-20 17:17:23 +0300

    modified the db connection file to work on multiple servers

--- inc/db.php
+++ inc/db.php
@@ -1,6 +1,12 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'apa');
+if ($_SERVER['DOCUMENT_ROOT'] == '/var/www/html') {
+	$conn = mysqli_connect('localhost', 'root', 'VDW-pNs-Mk6-gLQ', 'apa_website');
+} elseif ($_SERVER['DOCUMENT_ROOT'] == 'C:/xampp/htdocs') {
+	$conn = mysqli_connect('localhost', 'root', '', 'apa');
+} else {
+	$conn = mysqli_connect('localhost', 'root', '', 'apa');
+}
 if (!$conn) {
-	echo "Error connceting to database " . errorno();
+	echo "Error connceting to database: " .  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 }
 date_default_timezone_set("Africa/Nairobi");
