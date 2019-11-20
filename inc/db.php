729542f8362ed7900f5d21a7671454bda8a077bc
anthony-baru <anthonybaru@gmail.com>
2019-11-20 10:12:49 +0300

    removed db test super globals

--- inc/db.php
+++ inc/db.php
@@ -4,5 +4,3 @@ if (!$conn) {
 	echo "Error connceting to database " . errorno();
 }
 date_default_timezone_set("Africa/Nairobi");
-echo '<h1>'.$_SERVER['DOCUMENT_ROOT'].'</h1><br>';
-echo '<h1>'.$_SERVER['HTTP_HOST'].'</h1>';
