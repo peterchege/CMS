646fd35a5c115b23cc7e5863691e4c3a5eefdd3e
anthony-baru <anthonybaru@gmail.com>
2019-11-20 10:09:30 +0300

    added modification on ckeditor upload php script

--- upload.php
+++ upload.php
@@ -9,11 +9,15 @@ if (isset($_FILES['upload']['name'])) {
     $extension = end($file_name_array);
     $new_image_name = rand() . '.' . $extension;
     chmod('upload', 0777);
+    if ($_SERVER['DOCUMENT_ROOT'] == 'C:/xampp/htdocs') {
+        $urlD = 'http://localhost/cms/upload/';
+    }
+
     $allowed_extension = array("jpg", "gif", "png");
     if (in_array($extension, $allowed_extension)) {
         move_uploaded_file($file, 'upload/' . $new_image_name);
         $function_number = $_GET['CKEditorFuncNum'];
-        $url = 'http://localhost/cms/upload/' . $new_image_name;
+        $url = $urlD . $new_image_name;
         $message = '';
         echo "<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction($function_number, '$url', '$message');</script>";
     }
